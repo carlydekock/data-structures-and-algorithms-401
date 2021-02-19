3f160a786a0fb5ea1d1b25f4ee596277ab22b9d1
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-02-19 15:52:51 -0800

    fixed arrayReverse and arrayShift challenges

--- challenges/arrayReverse/array-reverse.js
+++ challenges/arrayReverse/array-reverse.js
@@ -4,7 +4,8 @@ function reverseArray(arr) {
   // console.log(arr);
   const result = [];
   for(let i = arr.length -1; i >= 0; i--){
-    result.push(arr[i]);
+    // result.push(arr[i]);
+    result[result.length] = arr[i];
   }
   // console.log(result);
   return result;
